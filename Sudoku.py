c807b56bd4101bcbc3ed61ddf3b24e76cee11150
BeauSLM <mccartney.beausl@gmail.com>
2021-06-04 11:09:51 -0600

    Add documentation, rename some symbols

--- Sudoku.py
+++ Sudoku.py
@@ -2,26 +2,30 @@
 # Beau McCartney
 # June 2021
 
-# file input
+# Create file handle
 def init_file():
     return open('lib/' + input("Enter file name: "), 'r')
 
-def init_matrix(fhand):
-    matrix = [[0 for i in range(9)] for j in range(9)]
+# Read in contents of the file, into a 9x9 list
+def init_board(fhand):
+    # initialize a list with nine elements, each element is a list with 9 integer elements
+    board = [[0 for i in range(9)] for j in range(9)]
+
+    # Populate the list with the puzzle values in the file
     i = 0
     j = 0
     for line in fhand:
         line = line.strip()
         for number in line:
-            matrix[i][j] = int(number)
+            board[i][j] = int(number)
             j += 1
         i += 1
         j = 0
     print("Puzzle:" )
-    for row in matrix:
+    for row in board:
         print(row)
-    return matrix
+    return board
 
 if __name__ == "__main__":
     fhand = init_file()
-    matrix = init_matrix(fhand)
+    matrix = init_board(fhand)
